da84bb9d5d016e1bd30060d28f8e7f945c253066
Muhammad-Niaz-5 <Zainsarfaraz1996@gmail.com>
2018-03-26 19:51:48 +0500

    first commit

---
+++ Inheritance/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Marksheet
+    {
+        protected int[] subject = new int[6];
+        protected double prcnt;
+        protected float avg; 
+        protected string name;
+        public string naam
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void Info()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine("\nEnter The Marks of Subject " + (i + 1)+":");
+                subject[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+    class percentage : Marksheet
+    {
+        public void average()
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                avg += subject[i];   
+            }
+            prcnt = avg*100 / 600;
+            avg = avg / 6;
+        }
+    }
+    class display : percentage
+    {
+        public void result()
+        {
+            Console.WriteLine("\n"+name+" Percentage is: ");
+            Console.Write(prcnt);
+            Console.WriteLine("\nAverage of the Subjects is: ");
+            Console.Write(avg + "\n");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("SIR SYED SYED UNIVERSITY OF ENGINEERING AND TECHNOLOGY");
+            Console.WriteLine("\nEnter Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("\nEnter Roll No: ");
+            int roll =int.Parse (Console.ReadLine());
+            Console.WriteLine("\nEnter Section: ");
+            string sec = Console.ReadLine();
+            Console.WriteLine("\nEnter Batch: ");
+            int batch = int.Parse(Console.ReadLine());
+            display marks = new display();
+            marks.naam = name;
+            Console.WriteLine("\nEnter Department Name: ");
+            string depart = Console.ReadLine();
+            if (depart == "Architecture Dept." || depart == "Computer Science Dept." || depart == "Arts Dept." || depart == "Commerce Dept.")
+            {
+                marks.Info();
+                marks.average();
+                marks.result();
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid Dept. Name!! ");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Inheritance/OOP Lab 6/OOP Lab 6/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOP_Lab_6
+{
+    class Account
+    {
+        protected double accountBalance;
+        public int transaction = 0;
+        public double account
+        {
+            get
+            {
+                return accountBalance;
+            }
+            set
+            {
+                accountBalance = value;
+            }
+        }
+        public void credit()
+        {
+
+            Console.WriteLine("Enter Amount to be Deposited: ");
+            double credit_Amount = int.Parse(Console.ReadLine());
+            accountBalance = accountBalance + credit_Amount;
+            transaction++;
+        }
+        public void debit()
+        {
+
+            Console.WriteLine("Enter Amount to be Withdrawn: ");
+            double debit_amount = int.Parse(Console.ReadLine());
+            if (debit_amount > accountBalance)
+            {
+
+                Console.WriteLine("\nDebit Amount is Higher!! ");
+            }
+            else
+            {
+                accountBalance = accountBalance - debit_amount;
+                transaction++;
+                Console.WriteLine("Number of Transaction: " + transaction);
+            }
+        }
+        public double getBalance()
+        {
+            return accountBalance;
+        }
+    }
+    class SavingsAccount : Account
+    {
+        public double InterestRate = 0.09;
+        public double CalculateInterest()
+        {
+
+            return (accountBalance * InterestRate);
+        }
+    }
+    class CheckingAccount : Account
+    {
+        public int Show_transaction()
+        {
+            return transaction;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            double get_acc;
+            Account a = new Account();
+            Console.WriteLine("*****Bank Al-Habib*****");
+            Console.WriteLine("Hello Sir, Good Morning.");
+            Console.WriteLine("\nEnter Your Total Amount in The Bank: ");
+            get_acc = double.Parse(Console.ReadLine());
+            a.account = get_acc;
+            while (i == 0)
+            {
+                Console.WriteLine("1. Deposit Amount ");
+                Console.WriteLine("2. Withdraw Amount ");
+                Console.WriteLine("3. Show Interest Rate ");
+                Console.WriteLine("Enter Your Choice: ");
+                int choice = int.Parse(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        a.credit();
+                        Console.WriteLine("New Amount: " + a.getBalance());
+                        break;
+                    case 2:
+                        a.debit();
+                        Console.WriteLine("Amount left: " + a.getBalance());
+                        break;
+                    case 3:
+                        SavingsAccount sa = new SavingsAccount();
+                        sa.CalculateInterest();
+                        CheckingAccount ca = new CheckingAccount();
+                        Console.WriteLine("Amount charged per Transaction: " + (a.transaction * 20));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input!! ");
+                        break;
+
+                }
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
